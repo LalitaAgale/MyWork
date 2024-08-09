be689b2699d02a06b825120b6b9fe8962ae26da6
lalita <laleetaagale@gmail.com>
2024-08-09 14:18:30 +0530

    two

--- gitpro/src/main/java/gitpro/practice1.java
+++ gitpro/src/main/java/gitpro/practice1.java
@@ -1,11 +1,26 @@
 package gitpro;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class practice1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("sss");
-System.out.println("my name is lali");
+		String str="llaahhttfb";
+		int s=str.length();
+		Set<Character>set=new LinkedHashSet<>();
+		StringBuilder sb=new StringBuilder();
+		for(int i=0;i<=s-1;i++) {
+			set.add(str.charAt(i));
+				
+			}
+		for(Character c:set) {
+			sb.append(c);
+		}
+		System.out.println(sb);
+		}
 	}
 
-}
+
+
+
