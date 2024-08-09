a7a3761ce3dc923852906e24de339711bbe827f1
lalita <laleetaagale@gmail.com>
2024-08-09 14:43:59 +0530

    newone

--- gitpro/src/main/java/gitpro/practice1.java
+++ gitpro/src/main/java/gitpro/practice1.java
@@ -18,8 +18,24 @@ public class practice1 {
 			sb.append(c);
 		}
 		System.out.println(sb);
+		
+		int []a= {8,7,6,5,4,3,2,1};
+		int k=3;
+		for(int i=0;i<a.length;i++) {
+			for(int j=1+i;j<a.length;j++) {
+				if(a[i]>a[j]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			System.out.print(a[i]+",");
+			
 		}
-	}
+		}
+	
+		}
+	
 
 
 
