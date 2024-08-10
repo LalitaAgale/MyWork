54ad47f443e0fa9d2b7c9e78f01c40ced7c6f7ff
shaurya123 <laleetaagale@gmail.com>
2024-08-10 15:09:12 +0530

    message to github

--- gitpro/src/test/java/gitpro/Oddeven.java
+++ gitpro/src/test/java/gitpro/Oddeven.java
@@ -4,6 +4,14 @@ public class Oddeven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String abc="Deepa";
+		int c=abc.length();
+		for(int i=c-1;i>=0;i--)
+		{
+			System.out.print(abc.charAt(i));
+		}
+		
 
 	}
 
